b12c131a15e40698ccbb9eb1200660975eed530a
robertoamin <r.amin@uniandes.edu.co>
2022-11-06 10:34:40 -0500

    se calculan los tipos de cafe encontrados en el API

--- src/app/cafes/cafes.component.ts
+++ src/app/cafes/cafes.component.ts
@@ -12,13 +12,41 @@ export class CafesComponent implements OnInit {
 
 
   constructor(private cafeService: CafeService) { }
-    cafes:Array<Cafe>=[];
+  cafes:Array<Cafe>=[];
+  tiposCafes:Array<string>=[];
+  cantidadCafes:Array<number>=[];
 
   getCafes():void {
-      this.cafeService.getCafes().subscribe((cafes)=>{ this.cafes =cafes;});
+      this.cafeService.getCafes().subscribe((cafes)=>{ this.cafes =cafes; this.getTipoCafes(this.cafes);});
+
     }
+  getTipoCafes(cafes:any):void {
+
+    let resultado=[];
+
+    const tempArray = [...cafes]; //definimos el arreglo sobre el q trabajaremos
+    const unique = [...new Set(tempArray.map(item => item.tipo))];//encontramos los tipos de cafe existentes
+    for (let j=0; j<unique.length; j++){
+      resultado.push(0); //iniciamos contadores
+    }
+
+    for (let i=0;i<tempArray.length;i++){
+      for (let j=0; j<unique.length;j++){
+        if (tempArray[i].tipo===unique[j]){
+          resultado[j]++;
+        }//vamos a recorrer el arreglo tempArray buscando cuantos unique hay
+      }
+    }
+    this.tiposCafes=unique;
+    this.cantidadCafes=resultado;
+
+  }
+
   ngOnInit() {
     this.getCafes();
+
+
+
   }
 
 }
